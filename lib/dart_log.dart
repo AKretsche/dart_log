7f808be00924d238421410eaa515b47d031d24c8
Andreas Kretschmer <akr@icnh.de>
2022-08-13 11:49:00 +0200

    Implement log

--- lib/dart_log.dart
+++ lib/dart_log.dart
@@ -1,7 +1,61 @@
 library dart_log;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:developer' as developer;
+
+import 'package:flutter/foundation.dart';
+
+/// Prints a colored log to the debug console.
+/// You can disable all log prints globally by setting the instance [enabled] to false.
+class Log {
+  Log._();
+
+  static final instance = Log._();
+
+  bool enabled = true;
+
+  /// Debug log
+  static void d(String log, {String? identifier, LogColor color = LogColor.cyan}) {
+    instance.log(log, identifier: identifier, color: color);
+  }
+
+  static void success(String log, {String? identifier}) {
+    instance.log(log, identifier: identifier, color: LogColor.green);
+  }
+
+  static void error(String log, {String? identifier}) {
+    instance.log(log, identifier: identifier, color: LogColor.red);
+  }
+
+  static void warning(String log, {String? identifier}) {
+    instance.log(log, identifier: identifier, color: LogColor.yellow);
+  }
+
+  @visibleForTesting
+  String? log(String log, {String? identifier, LogColor color = LogColor.white}) {
+    String? output;
+    if (enabled) {
+      final prefix = identifier != null //
+          ? '$identifier: '
+          : '';
+      output = '${color.code}$prefix$log${LogColor.reset.code}';
+      developer.log(output);
+    }
+    return output;
+  }
+}
+
+enum LogColor {
+  @visibleForTesting
+  reset('\x1B[0m'),
+  black('\x1B[30m'),
+  red('\x1B[31m'),
+  green('\x1B[32m'),
+  yellow('\x1B[33m'),
+  blue('\x1B[34m'),
+  cyan('\x1B[36m'),
+  white('\x1B[37m');
+
+  const LogColor(this.code);
+
+  final String code;
 }
